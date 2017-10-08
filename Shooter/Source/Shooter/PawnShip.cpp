b9877f0547d3616437eb6cea641dab64c1db4f85
Lafon Sylvain <slafon@manzalab.com>
2017-10-08 19:56:13 +0200

    Improves ship's camera settings

--- Shooter/Source/Shooter/PawnShip.cpp
+++ Shooter/Source/Shooter/PawnShip.cpp
@@ -10,6 +10,9 @@
 // Sets default values
 APawnShip::APawnShip()
 {
+	this->BaseMaxSpeed = 1200;
+	this->BaseAcceleration = 4000;
+
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -18,24 +21,21 @@ APawnShip::APawnShip()
 	BaseCollision->InitBoxExtent(FVector(50, 50, 50));
 	BaseCollision->CanCharacterStepUpOn = ECB_No;
 	
-
 	USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachmentArm"));
 	SpringArm->SetupAttachment(RootComponent);
-	SpringArm->RelativeRotation = FRotator(-45.f, 0.f, 0.f);
-	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->RelativeRotation = FRotator(-90.f, 0.f, 0.f);
+	SpringArm->TargetArmLength = 800.0f;
 	SpringArm->bEnableCameraLag = true;
-	SpringArm->CameraLagSpeed = 3.0f;
-
+	SpringArm->CameraLagSpeed = 10.0f;
+	SpringArm->bDoCollisionTest = false;
+	
 	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("ActualCamera"));
 	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 
 	this->MovementComponent = CreateDefaultSubobject<UFloatingPawnMovementShip>(TEXT("ShipMovement"));
 	this->MovementComponent->UpdatedComponent = RootComponent;
-
-	this->MovementComponent->Acceleration		= this->BaseAcceleration;
-	this->MovementComponent->MaxSpeed			= this->BaseMaxSpeed;
-
-	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	
+	//AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 	//Mesh = CreateDefaultSubobject<UMeshComponent>(TEXT("ShipMesh"));
 	//if (Mesh)
@@ -49,7 +49,8 @@ APawnShip::APawnShip()
 void APawnShip::BeginPlay()
 {
 	Super::BeginPlay();
-
+	this->MovementComponent->Acceleration	= this->BaseAcceleration;
+	this->MovementComponent->MaxSpeed		= this->BaseMaxSpeed;
 }
 
 // Called every frame
